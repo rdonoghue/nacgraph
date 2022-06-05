e2cc9f50ddd175ece239ece0877082f23f63f2ec
BuildTools <unconfigured@null.spigotmc.org>
2022-06-05 18:38:07 -0400

    This is a pile of hot garbage

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nacgraph
+
 Trying to make a diagram for incar
+
+## Notes
+
+- Trying out [Graphology](https://graphology.github.io)
+- Ok apparenlty I can't even successfully import it after a node install, and I'm way too ignoran tto troubleshoot this beyond what googling has found me. I've tried every variant of calling 'import' I could fine
---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import { Graph } from "graphology";
+
+// import Graph from "graphology";
+// var Graph = import("./node_modules/graphology");
+
+const graph = new Graph();
+
+// // Adding some nodes
+// graph.addNode("John");
+// graph.addNode("Martha");
+
+// // Adding an edge
+// graph.addEdge("John", "Martha");
+
+// // Displaying useful information about your graph
+// console.log("Number of nodes", graph.order);
+// console.log("Number of edges", graph.size);
+
+// // Iterating over nodes
+// graph.forEachNode((node) => {
+//   console.log(node);
+// });
